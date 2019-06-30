ef13af08895d3855a034ac46c71f0fbd9c13ab89
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2019-06-30 09:34:27 +0200

    aggiornata funzione di loss

--- nets/AlexNet/loss_ops.py
+++ nets/AlexNet/loss_ops.py
@@ -10,6 +10,9 @@ def cross_entropy_loss(labels_tensor, logits_tensor):
             It should have the same dimensions as labels_tensor
     :return: Cross-entropy Loss tensor
     """
-    diff = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits_tensor, labels=labels_tensor)
+    # use softmax_cross_entropy_with_logits instead of softmax_cross_entropy_with_logits_v2 because our labels are
+    # mutually exclusive and one class at a time.
+    diff = tf.losses.softmax_cross_entropy(logits=logits_tensor, onehot_labels=labels_tensor)
+    # diff = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits_tensor, labels=labels_tensor)
     loss = tf.reduce_mean(diff)
     return loss
