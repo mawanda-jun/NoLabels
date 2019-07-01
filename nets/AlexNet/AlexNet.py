7a03c8e2901e48cf69da40c5ac99717650ef47ab
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2019-07-01 16:24:01 +0200

    riportata alexnet a prima implementazione, con qualche modifica

--- nets/AlexNet/AlexNet.py
+++ nets/AlexNet/AlexNet.py
@@ -11,24 +11,65 @@ Comments: Includes function which creates the 3D ResNet with 50 layer.
 **********************************************************************************
 """
 
-from nets.AlexNet.ops import conv_2d, flatten_layer, fc_layer, dropout, max_pool, lrn
+from nets.AlexNet.ops import conv_2d, flatten_layer, fc_layer, dropout, max_pool, lrn, batch_norm_wrapper
+from tensorflow.python.keras import layers
 
 
-def AlexNet(X, rate, is_train):
-    net = conv_2d(X, 11, 2, 96, 'CONV1', trainable=True, is_train=is_train)
+def AlexNet(X, rate, is_train, use_bn):
+    """
+    It is important to call the function since the "reuse" method is not working in keras yet. This way it is supposed
+    to call the class for each layer and to share weights. I stated that since the net is not exploding while calling
+    the methods in this way.
+    :param X:
+    :param rate:
+    :param is_train:
+    :return:
+    """
+    net = conv_2d(X, 11, 2, 96, 'CONV1', is_train=is_train, padding="VALID")
     net = max_pool(net, 3, 2, 'MaxPool1')
-    net = lrn(net)
-    net = conv_2d(net, 5, 2, 256, 'CONV2', trainable=True, is_train=is_train)
+    if use_bn:
+        net = batch_norm_wrapper(net, is_train, decay=0.7)
+    else:
+        net = lrn(net)
+
+    net = conv_2d(net, 5, 2, 256, 'CONV2', is_train=is_train)
     net = max_pool(net, 3, 2, 'MaxPool2')
-    net = lrn(net)
-    net = conv_2d(net, 3, 1, 384, 'CONV3', trainable=True, is_train=is_train)
-    net = conv_2d(net, 3, 1, 384, 'CONV4', trainable=True, is_train=is_train)
-    net = conv_2d(net, 3, 1, 256, 'CONV5', trainable=True, is_train=is_train)
+    if use_bn:
+        net = batch_norm_wrapper(net, is_train, decay=0.7)
+    else:
+        net = lrn(net)
+
+    net = conv_2d(net, 3, 1, 384, 'CONV3', is_train=is_train)
+    net = conv_2d(net, 3, 1, 384, 'CONV4', is_train=is_train)
+    net = conv_2d(net, 3, 1, 256, 'CONV5', is_train=is_train)
     net = max_pool(net, 3, 2, 'MaxPool3')
+
     layer_flat = flatten_layer(net)
-    net = fc_layer(layer_flat, 512, 'FC6', trainable=True, is_train=is_train)
+    net = fc_layer(layer_flat, 1024, 'FC6', is_train=is_train)
+
     net = dropout(net, rate, is_train)
+
     return net
+
+    # net = layers.Conv2D(96, 11, 2, 'same', activation='relu', name='CONV1')(X)
+    # # net = layers.BatchNormalization()(net, training=is_train)
+    # net = lrn(net)
+    # net = layers.MaxPool2D(3, 2, 'same', name='MaxPool1')(net)
+    #
+    # net = layers.Conv2D(256, 5, 2, 'same', activation='relu', name='CONV2')(net)
+    # # net = layers.BatchNormalization()(net, training=is_train)
+    # net = lrn(net)
+    # net = layers.MaxPool2D(3, 2, 'same', name='MaxPool2')(net)
+    #
+    # net = layers.Conv2D(384, 3, 1, 'same', activation='relu', name='CONV3')(net)
+    # net = layers.Conv2D(384, 3, 1, 'same', activation='relu', name='CONV4')(net)
+    # net = layers.Conv2D(256, 3, 1, 'same', activation='relu', name='CONV5')(net)
+    # net = layers.MaxPool2D(3, 2, 'same', name='MaxPool3')(net)
+    #
+    # net = layers.Flatten()(net)
+    # net = layers.Dense(512, activation='relu', name='FC6')(net)
+    # net = layers.Dropout(rate)(net, training=is_train)
+    # return net
 
 
 def AlexNet_target_task(X, keep_prob, num_cls):
