658d85ce6fa1dda4435b1c9480dcd9efe77c23c3
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2019-07-05 18:39:56 +0200

    FIGATA

--- nets/Siamese_2.0.py
+++ nets/Siamese_2.0.py
@@ -1,31 +1,7 @@
 import tensorflow as tf
 tf.enable_eager_execution()
-from tensorflow.python.keras import layers, losses, models
-from typing import List, Dict, Tuple
-import numpy as np
-
-
-# placeholder for params
-class Params:
-    @property
-    def dropout_rate(self):
-        return None
-
-
-params = Params()
-shared_layers = {
-    'CONV1': layers.Conv2D(96, 11, 2, 'same', activation='relu', name='CONV1'),
-    'FC6': layers.Dense(512, activation='relu', name="FC6")
-}
-
-X_train = np.random.random((3, 256, 256, 3, 9))
-y_train = np.random.random((3, 5))
-X_test = np.random.random((3, 256, 256, 3, 9))
-y_test = np.random.random((3, 5))
-
-batch_size = 2
-epochs = 2
-num_classes = 10
+from tensorflow.python.keras import layers
+from inspect import signature
 
 
 class AlexNet(tf.keras.Model):
@@ -34,7 +10,7 @@ class AlexNet(tf.keras.Model):
     """
     def __init__(self, dropout_rate):
         super(AlexNet, self).__init__()
-        self.alexnet = models.Sequential([
+        self.alexnet = [
             layers.Conv2D(96, 11, 2, 'same', activation='relu', name='CONV1'),
             layers.MaxPool2D(3, 2, 'same', name='MaxPool1'),
 
@@ -49,10 +25,16 @@ class AlexNet(tf.keras.Model):
             layers.Flatten(),
             layers.Dense(512, activation='relu', name='FC6'),
             layers.Dropout(dropout_rate)
-        ])
+        ]
 
     def call(self, inputs, training=None, mask=None):
-        return self.alexnet(inputs, training)
+        x = self.alexnet[0](inputs)
+        for layer in self.alexnet[1:]:
+            if 'training' not in str(signature(layer.call)):
+                x = layer(x)
+            else:
+                x = layer(x, training)
+        return x
 
 
 class Siamese(tf.keras.Model):
@@ -60,7 +42,7 @@ class Siamese(tf.keras.Model):
         super(Siamese, self).__init__()
         self.alex = AlexNet(dropout_rate)
         self.alex_block = [self.alex for _ in range(9)]
-        self.siamese = models.Sequential()
+        self.dense = layers.Dense(4096, activation='relu', name='FC7')
         self.classifier = layers.Dense(num_classes, name='FC8')
         self.dropout_rate = dropout_rate
 
@@ -73,23 +55,19 @@ class Siamese(tf.keras.Model):
         :return:
         """
         x = tf.unstack(inputs, axis=-1)
-        siamese = []
+        alexes = []
         for i in range(9):
             # it should reuse the variables inside alex_block
-            siamese.append(self.alex_block[i](x[i], training))
-        # siamese_block = layers.Concatenate(siamese)
-        siamese_block = tf.concat(siamese, axis=1)
+            alexes.append(self.alex_block[i](x[i], training))
+        siamese_block = tf.concat(alexes, axis=1)
 
-        self.siamese.add([
-            layers.Dense(4096, activation='relu',  name='FC7'),
-            layers.Dropout(self.dropout_rate),
-            self.classifier
-        ])
-        return self.siamese(siamese_block, training=training)
+        x = self.dense(siamese_block)
+        x = layers.Dropout(self.dropout_rate)(x, training)
+        return self.classifier(x)
 
 
 if __name__ == '__main__':
-    model = Siamese(dropout_rate=0.5, num_classes=num_classes)
+    model = Siamese(dropout_rate=0.5, num_classes=5)
     model.compile(optimizer=tf.train.AdamOptimizer(0.001),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
