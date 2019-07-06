731f3b7bb288837e743713402829c1b44ebdd36c
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2019-07-06 20:02:23 +0200

    aggiornato, pulito main. In attesa di una nuova classe

--- main_2.py
+++ main_2.py
@@ -6,8 +6,7 @@ import os
 from utils.logger import set_logger
 import logging
 from Dataset.crops_generator_TF import CropsGenerator
-from tensorflow.contrib.eager.python import tfe
-from tensorflow.python.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow.python.keras.callbacks import ModelCheckpoint, CSVLogger, TensorBoard
 from nets.Siamese_eager import Siamese
 
 tf.enable_eager_execution()
@@ -43,19 +42,22 @@ def train():
         train_set = data_reader.generate(mode='train')
         val_set = data_reader.generate(mode='val')
 
+        # set dirs per run
+        model_dir = os.path.join(os.getcwd(), conf.modeldir, conf.run_name)
+        log_dir = os.path.join(os.getcwd(), conf.logdir, conf.run_name)
+        save_dir = os.path.join(os.getcwd(), conf.savedir, conf.run_name)
         # set experiment path
-        os.makedirs(conf.modeldir + conf.run_name, exist_ok=True)
-        os.makedirs(conf.logdir + conf.run_name, exist_ok=True)
-        os.makedirs(conf.savedir + conf.run_name, exist_ok=True)
-        set_logger(os.path.join(os.getcwd(), 'train_val.log'))
+        os.makedirs(log_dir, exist_ok=True)
+        os.makedirs(model_dir, exist_ok=True)
+        os.makedirs(save_dir, exist_ok=True)
+        set_logger(os.path.join(log_dir, 'train_val.log'))
 
         # check if saved weights are present
-        experiment_path = conf.modeldir + conf.run_name
         if conf.reload_step > 0:
-            list_files = os.listdir(experiment_path)
+            list_files = os.listdir(model_dir)
             if len(list_files) > 0:
                 restore_filename = list_files[-1]
-                restore_path = os.path.join(experiment_path, restore_filename)
+                restore_path = os.path.join(model_dir, restore_filename)
                 if os.path.isfile(restore_path):
                     logging.info("Restoring weights in file {}...".format(restore_filename))
                     model.load_weights(restore_path)
@@ -80,7 +82,7 @@ def train():
         model.summary()
 
         # Setup callback to save best weights after each epoch
-        checkpointer = ModelCheckpoint(filepath=os.path.join(experiment_path,
+        checkpointer = ModelCheckpoint(filepath=os.path.join(model_dir,
                                                              'weights.{epoch:02d}-{val_loss:.2f}.hdf5'),
                                        verbose=0,
                                        save_best_only=True,
@@ -88,7 +90,17 @@ def train():
                                        monitor='val_loss',
                                        mode='min')
         # setup callback to register training history
-        csv_logger = CSVLogger('log.csv', append=True, separator=';')
+        csv_logger = CSVLogger(os.path.join(log_dir, 'log.csv'), append=True, separator=';')
+
+        # setup callback to retrieve tensorboard info
+        tensorboard = TensorBoard(log_dir=log_dir,
+                                  write_graph=True,
+                                  histogram_freq=0,
+                                  write_grads=True,
+                                  write_images=False,
+                                  # batch_size=conf.batchSize,
+                                  # batch_size * 10 because epoch takes too long and batch size too short
+                                  update_freq=conf.batchSize * 10)
 
         # fit and validate model
         model.fit(
@@ -98,7 +110,7 @@ def train():
             steps_per_epoch=data_reader.num_train_batch,
             validation_steps=data_reader.num_val_batch,
             verbose=1,
-            callbacks=[checkpointer, csv_logger]
+            callbacks=[checkpointer, csv_logger, tensorboard]
         )
 
 
